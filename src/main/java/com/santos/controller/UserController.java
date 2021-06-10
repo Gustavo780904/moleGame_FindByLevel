1d78c088e9f4c7530794d832d8871dfe21b1e09c
Gustavo780904 <gustavodutrasantos@gmail.com>
2021-06-10 09:37:42 -0300

    metodos delete

--- src/main/java/com/santos/controller/UserController.java
+++ src/main/java/com/santos/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -88,5 +89,11 @@ public class UserController {
 		User scores = userService.getById(id);
 		scores.addScore(score);
 		return rankingService.newScore(score);
+	}
+	@CrossOrigin
+	@DeleteMapping("/{id}")
+	public User delete(@PathVariable Long id) {
+		User users = userService.deleteAll();
+		return userService.deleteAll();
 	}
 }
